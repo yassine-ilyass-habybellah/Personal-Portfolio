d08f25c6a16b0477b5edf957db90e8554e39cd6c
charliexa <derrhalo50@gmail.com>
2023-08-10 02:32:00 +0100

    push test

--- js/main.js
+++ js/main.js
@@ -8,5 +8,6 @@ function activateIt(clicked_id) {
   let activeNav = document.querySelector(".nav-bar ul .active")
   activeNav.classList.remove("active")
   clicked_id.classList.add("active")
+  console.log("satadgaldh")
 }
 
