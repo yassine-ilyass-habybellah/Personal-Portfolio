f712eb6f8e9d54c2877837ec769e0f0cb4a7e9ba
charliexa <derrhalo50@gmail.com>
2023-08-10 02:11:54 +0100

    upload files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let about = document.getElementById("about")
+let resume = document.getElementById("resume")
+let works = document.getElementById("works")
+let contact = document.getElementById("contact")
+
+// Make the active section colored
+function activateIt(clicked_id) {
+  let activeNav = document.querySelector(".nav-bar ul .active")
+  activeNav.classList.remove("active")
+  clicked_id.classList.add("active")
+}
+
