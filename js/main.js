092729b620cd70e38b08d4171206246433392bbd
Charli_x1 <derrhalo50@gmail.com>
2023-10-22 18:02:26 +0100

    responsive

--- js/main.js
+++ js/main.js
@@ -9,33 +9,50 @@ let variables = {
   cardcontact: document.querySelector(".contact-content-container")
 };
 let order = document.querySelector(".order1")
+let aboutButton = document.getElementById('about-button');
+let resumeButton = document.getElementById('resume-button');
+let worksButton = document.getElementById('works-button');
+let contactButton = document.getElementById('contact-button');
 
 function activateIt(clicked_id) {
   // Make the active section colored
-    let activeNav = document.querySelector(".nav-bar ul .active") // check for the previous active nav
-    activeNav.classList.remove("active") // remove the class
-    clicked_id.classList.add("active") // add it to the active currently
-    let Currentcard = variables["card" + `${clicked_id.id}`]
-    let Previouscard = variables["card" + `${activeNav.id}`]
-    Currentcard.classList.remove("fade-in-down")
-    Currentcard.classList.remove("fade-out-up")
-    if (Currentcard === Previouscard) {
-      return
+    const currentWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const maxWidth = 1107
+    if (currentWidth >= maxWidth) {
+      aboutButton.setAttribute('href', '#');
+      resumeButton.setAttribute('href', '#');
+      worksButton.setAttribute('href', '#');
+      contactButton.setAttribute('href', '#');
+      let activeNav = document.querySelector(".nav-bar ul .active") // check for the previous active nav
+      activeNav.classList.remove("active") // remove the class
+      clicked_id.classList.add("active") // add it to the active currently
+      let Currentcard = variables["card" + `${clicked_id.id}`]
+      let Previouscard = variables["card" + `${activeNav.id}`]
+      Currentcard.classList.remove("fade-in-down")
+      Currentcard.classList.remove("fade-out-up")
+      if (Currentcard === Previouscard) {
+        return
+      } else {
+        console.log("Current:",Currentcard)
+        console.log("Previous:", Previouscard)
+        console.log("###############")
+        Currentcard.classList.remove("hidden")
+        Previouscard.classList.remove("current")
+        Currentcard.classList.add("fade-in-down")
+        Previouscard.classList.add("fade-out-up")
+        Currentcard.classList.add("current")
+        Previouscard.classList.add("hidden")
+    }
+    const activeElements = document.querySelectorAll(".nav-bar ul .active");
+    if (activeElements.length === 0) {
+      contact.classList.add("active")
     } else {
-      console.log("Current:",Currentcard)
-      console.log("Previous:", Previouscard)
-      console.log("###############")
-      Currentcard.classList.remove("hidden")
-      Previouscard.classList.remove("current")
-      Currentcard.classList.add("fade-in-down")
-      Previouscard.classList.add("fade-out-up")
-      Currentcard.classList.add("current")
-      Previouscard.classList.add("hidden")
-  }
-  const activeElements = document.querySelectorAll(".nav-bar ul .active");
-  if (activeElements.length === 0) {
-    contact.classList.add("active")
-  } else {
-    return
+      return
+    }
+  } else if (currentWidth < maxWidth) {
+    // aboutButton.setAttribute('href', '#about-section');
+    // resumeButton.setAttribute('href', '#resume-section');
+    // worksButton.setAttribute('href', '#works-section');
+    // contactButton.setAttribute('href', '#works-section');
   }
 }
